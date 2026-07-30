0562ec30c926f9a97d511e8d05de3806d712ec3d
Jacob <muskins18@gmail.com>
2026-07-30 04:46:23 -0400

    create common way of calling prompts

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import json
 
+
 from prompts.find_person import choose_person
+from prompts.research_person import research_person
 # from prompts.research_person import research_person
 
 with open("data/sample_user.json", "r") as file:
@@ -8,5 +10,17 @@ with open("data/sample_user.json", "r") as file:
 
 
 person = choose_person(user_profile)
-print(person["name"])
-print(person["reason"])
+
+print(person.name)
+print(person.reason)
+
+recommendations = research_person(person, user_profile["interests"])
+
+print(recommendations.fun_fact)
+
+for lesson in recommendations.lessons:
+    print(lesson.lesson)
+    print(lesson.tags)
+
+print(recommendations.book_recommendation.title)
+print(recommendations.book_recommendation.author)
