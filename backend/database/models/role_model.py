fc6ac581e0ba3ea4460a27171b66bee9a6885817
Jacob <muskins18@gmail.com>
2026-08-06 07:47:48 -0400

    add db model for role model and tags

--- backend/database/models/role_model.py
+++ backend/database/models/role_model.py
@@ -1,18 +1,23 @@
 
+
 """
     Columns:
         name
+        reason
 """
-
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, JSON
 
 from database.base import Base
 
-class Tag(Base):
+class RoleModel(Base):
 
-    __tablename__ = "tags"
+    __tablename__ = "role_models"
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    name: Mapped[str] = mapped_column(String(100), unique=True, index=True)
+    name: Mapped[str] = mapped_column(String(100))
+
+    fun_fact: Mapped[str] = mapped_column(String(200))
+
+    
