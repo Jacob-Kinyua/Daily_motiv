84f8207a0f9d06d5fbc753f47615eadd7e31ca6b
Jacob <muskins18@gmail.com>
2026-08-12 08:34:46 -0400

    add simple recommendation engine

--- backend/services/recommendation-service.py
+++ backend/services/recommendation-service.py
@@ -8,6 +8,7 @@ from backend.database.models.user import User
 from backend.database.models.recommendation import Recommendation
 from backend.database.models.tag import Tag
 from backend.database.models.role_model_tag_score import RoleModelTagScore
+from .role_model_service import get_role_model
 
 
 def create_recommendation(
@@ -31,18 +32,6 @@ def create_recommendation(
     return recommendation
 
 
-def get_recommendation(
-    session: Session,
-    recommendation_id: int
-) -> Recommendation | None:
-
-    statement = select(Recommendation).where(
-        Recommendation.id == recommendation_id
-    )
-
-    return session.scalars(statement).first()
-
-
 def get_user_recommendations(
     session: Session,
     user_id: int
@@ -57,15 +46,42 @@ def get_user_recommendations(
     return list(session.scalars(statement).all())
 
 
-# def rank_role_models(tags):
-#     role_models = select(Role)
+def rank_role_models(session: Session, user: User, role_models: list[RoleModel]):
+    rankings = []
 
+    for role_model in role_models:
 
-def get_unseen_role_models(
-    session: Session,
-    user: User,
-    limit: int = 10
-) -> list[RoleModel]:
+        statement = (
+            select(RoleModelTagScore)
+            .join(Tag)
+            .where(
+                RoleModelTagScore.role_model_id == role_model.id,
+                Tag.name.in_(user.interests)
+            )
+        )
+
+        scores = session.scalars(statement).all()
+
+        total_score = sum(score.score for score in scores)
+
+        rankings.append({
+            "role_model_id": role_model.id,
+            "score": total_score
+        })
+
+    return sorted(
+        rankings,
+        key=lambda x: x["score"],
+        reverse=True
+    )
+
+def select_role_model(ranked_role_models):
+    if not ranked_role_models:
+        return None
+
+    return ranked_role_models[0]
+
+def get_unseen_role_models(session: Session, user: User) -> list[RoleModel]:
 
     seen_role_models = (
         select(Recommendation.role_model_id)
@@ -80,7 +96,21 @@ def get_unseen_role_models(
             Tag.name.in_(user.interests),
             ~RoleModel.id.in_(seen_role_models)
         )
-        .limit(limit)
     )
 
-    return list(session.scalars(statement).unique().all())
+    return  list(session.scalars(statement).unique().all())
+
+
+def recommend_role_model(session: Session, user: User):
+
+    candidates = get_unseen_role_models(session,user)
+
+    ranked = rank_role_models(session,user,candidates)
+
+    selected = select_role_model(ranked)
+
+    if selected is None:
+        return None
+
+    return get_role_model(session,selected["role_model_id"])
+
