935fcd9f13ec47cb9e30cedd045ec8f26409c51a
Jacob <muskins18@gmail.com>
2026-08-13 04:43:39 -0400

    to test whether an email is sent

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,9 @@ from prompts.score_person import score_person
 from prompts.curate_response import curate_response
 from data.models import RoleModelResponse
 from services.role_model_service import *
+from services.user_service import *
+from services.role_model_service import *
+from services.generate_email import *
 
 AVAILABLE_TAGS = [
     "Leadership",
@@ -25,38 +28,39 @@ with open("data/sample_user.json", "r") as file:
     user_profile = json.load(file)
 
 
-existing_people = get_role_model_names()
+# existing_people = get_role_model_names()
+existing_people = []
 
 person = choose_person(user_profile, existing_people)
-
-print(person.name)
-print(person.reason)
-
 person_details = research_person(person, AVAILABLE_TAGS)
-print(person_details.fun_fact)
-
-for lesson in person_details.lessons:
-    print(lesson.lesson)
-    print(lesson.tags)
-
-print(person_details.book_recommendation.title)
-print(person_details.book_recommendation.author)
-
 person_score = score_person(person, AVAILABLE_TAGS)
-print(person_score.tag_scores)
 
 
-role_model_profile = RoleModelResponse(
-    name=person.name,
-    fun_fact=person_details.fun_fact,
-    lessons=person_details.lessons,
-    book_recommendation=person_details.book_recommendation,
-    tag_scores=person_score.tag_scores
-)
+# role_model_profile = RoleModelResponse(
+#     name=person.name,
+#     fun_fact=person_details.fun_fact,
+#     lessons=person_details.lessons,
+#     book_recommendation=person_details.book_recommendation,
+#     tag_scores=person_score.tag_scores
+# )
 
 
 
 
 curated_response = curate_response(user_profile, person, person_details)
+
+print("Person:", person.name)
+print("Reason:", person.reason)
+
+print("\nEmail subject:")
 print(curated_response.subject)
+
+print("\nEmail body:")
 print(curated_response.body)
+
+sent_email = send_email("gmail.com", curated_response.subject, curated_response.body)
+
+if sent_email:
+    print("sent email")
+else:
+    print("an error occurred")
